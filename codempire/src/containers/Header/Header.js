c7a0b780eb9475128618e969fb843e3206a49780
Vladymyr Radionov <radionovdon@gmail.com>
2021-08-28 12:35:09 +0300

    initt

---
+++ codempire/src/containers/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.scss';
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <div className="header__inner">
+                    <div className="header__logo"></div>
+                    {/* search */}
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
